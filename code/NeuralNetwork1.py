ae6788443f091145cadade788fcb53b2f4262b7c
Mohammad Hamed <45127743+M-AHamed@users.noreply.github.com>
2023-01-14 00:04:35 +0300

    gui2.0_filenotWorking

--- code/NeuralNetwork1.py
+++ code/NeuralNetwork1.py
@@ -1,7 +1,5 @@
-
 # ### Start from here
-# 
-
+#
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -16,18 +14,15 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
-
-
-
 # Load the data
-data = pd.read_csv("C:\\Users\\Mohammad\\Desktop\\Uni\\Uni work\\Year 4\\Term 7, fall 2022\\CMPE 460 Deep Learning\\project\\CMPE-460-Project\\dataset\\cars.csv",encoding='iso-8859-1')
+data = pd.read_csv(
+    "C:\\Users\\Mohammad\\Desktop\\Uni\\Uni work\\Year 4\\Term 7, fall 2022\\CMPE 460 Deep Learning\\project\\CMPE-460-Project\\dataset\\cars.csv",
+    encoding='iso-8859-1')
 
 
 
 # drop rows where the price is less than 500
 data = data[data['price'] >= 500]
-
-
 
 
 # # Drop the totalPrice column
@@ -36,20 +31,17 @@ data = data[data['price'] >= 500]
 # data = data[data['doornumber'].str.strip() !='']
 # data['doornumber'] = data['doornumber'].astype(int)
 
+# def check_missing_values(dataframe):
+#     missing_values = dataframe.isnull().any()
+#     if missing_values.any():
+#         print("Missing values in columns:")
+#         print(missing_values[missing_values == True])
+#     else:
+#         print("No missing values.")
 
-def check_missing_values(dataframe):
-    missing_values = dataframe.isnull().any()
-    if missing_values.any():
-        print("Missing values in columns:")
-        print(missing_values[missing_values==True])
-    else:
-        print("No missing values.")
+# check_missing_values(data)
 
-check_missing_values(data)
-
-
-
-data
+# data
 
 if 'Manufacturer' in data.columns:
     data = data.drop('Manufacturer', axis=1)
@@ -63,14 +55,16 @@ else:
 
 
 
-data
-
+# data
 
 # Define the list of categorical columns
-categorical_cols = ['symboling', 'Fuel type', 'aspiration', 'doornumber', 'Category', 'Drive wheels', 'enginelocation', 'enginetype', 'Cylinders', 'fuelsystem']
+categorical_cols = [
+    'symboling', 'Fuel type', 'aspiration', 'doornumber', 'Category',
+    'Drive wheels', 'enginelocation', 'enginetype', 'Cylinders', 'fuelsystem'
+]
 
 # Create the main array with car_ID column
-main_array = np.array(data['car_ID']).reshape(-1,1)
+main_array = np.array(data['car_ID']).reshape(-1, 1)
 
 # Iterate through columns in the dataframe
 for col in data.columns:
@@ -80,7 +74,7 @@ for col in data.columns:
         temp = np.array(pd.get_dummies(data[col]))
     else:
         # Otherwise, reshape the column
-        temp = np.array(data[col]).reshape(-1,1)
+        temp = np.array(data[col]).reshape(-1, 1)
     # Stack the column with the main array
     main_array = np.hstack((main_array, temp))
 
@@ -88,21 +82,17 @@ for col in data.columns:
 main_array = main_array[:, 2:]
 
 # Display the array as a dataframe
-pd.DataFrame(main_array)
-
-
+# pd.DataFrame(main_array)
 
 # Define the features and labels
 X = main_array[:, :-1]
 y = main_array[:, -1].reshape(-1, 1)
 
-
 for i, val in enumerate(X):
     for j, value in enumerate(val):
         if isinstance(value, str):
-            print("string value found in X at index [{}, {}]: {}".format(i, j, value))
-
-
+            print("string value found in X at index [{}, {}]: {}".format(
+                i, j, value))
 
 xScaler = StandardScaler()
 yScaler = StandardScaler()
@@ -117,15 +107,14 @@ print(pd.DataFrame(X_scaled).head())
 print("==========y_data after rescaling===============")
 print(Y_scaled.ravel())
 
-
-
-
-X_train, X_test, y_train, y_test = train_test_split(X_scaled, Y_scaled, test_size=0.1, shuffle=False)
+X_train, X_test, y_train, y_test = train_test_split(X_scaled,
+                                                    Y_scaled,
+                                                    test_size=0.1,
+                                                    shuffle=False)
 print("Shape of X_train: {}".format(X_train.shape))
 print("Shape of X_test: {}".format(X_test.shape))
 print("Shape of y_train: {}".format(y_train.shape))
 print("Shape of y_test: {}".format(y_test.shape))
-
 
 model = Sequential()
 model.add(Dense(8, activation='relu', input_shape=(None, 57)))
@@ -134,73 +123,131 @@ model.add(Dense(1, activation='linear'))
 model.compile(optimizer='adam', loss='mse', metrics=['mae'])
 model.summary()
 
-
-history = model.fit(x=X_train, y=y_train, epochs=100, validation_data=(X_test, y_test), verbose=0)
-
+history = model.fit(x=X_train,
+                    y=y_train,
+                    epochs=100,
+                    validation_data=(X_test, y_test),
+                    verbose=0)
 
 plt.plot(history.history['loss'], label="train_loss")
 plt.plot(history.history['val_loss'], label="val_loss")
 plt.legend()
 plt.show()
 
-
-PREDICT_ROW = 5
-predict_data = main_array[PREDICT_ROW, :].reshape(1, -1)
-X_predict = predict_data[:, :-1]
-y_true = predict_data[:, -1]
-predict_data_scaled = xScaler.transform(X_predict)
-y_pred_scaled = model.predict(predict_data_scaled)
-y_pred = yScaler.inverse_transform(y_pred_scaled)
-print("Prediction price result: {}".format(float(y_pred)))
-print("True price: {}".format(float(y_true)))
-print("Percentage error: {}".format(str(float(abs(y_true - y_pred) * 100 / y_true))))
-
-
-
+# # testing the output from row five as an input
+# PREDICT_ROW = 5
+# predict_data = main_array[PREDICT_ROW, :].reshape(1, -1)
+# print(main_array)
+# X_predict = predict_data[:, :-1]
+# y_true = predict_data[:, -1]
+# predict_data_scaled = xScaler.transform(X_predict)
+# y_pred_scaled = model.predict(predict_data_scaled)
+# y_pred = yScaler.inverse_transform(y_pred_scaled)
+# print("Prediction price result: {}".format(float(y_pred)))
+# print("True price: {}".format(float(y_true)))
+# print("Percentage error: {}".format(
+#     str(float(abs(y_true - y_pred) * 100 / y_true))))
 
 # #----------------------------------------------------------------------------------------------------------
 
-
 #def predict_price(features):
-    # Create a dataframe from the input features
-   # input_df = pd.DataFrame([features], columns=["symboling", "Fuel type", "aspiration", "doornumber", "Category", "Drive wheels", "enginelocation", "enginetype"])
-    # One-hot encode the categorical columns
-    #input_df = pd.get_dummies(input_df, columns=["symboling", "Fuel type", "aspiration", "doornumber", "Category", "Drive wheels", "enginelocation", "enginetype"])
-    # Fill in the missing values for the numerical columns
-    #input_df = input_df.fillna(input_df.mean())
-    # Scale the input features
-    #input_scaled = xScaler.transform(input_df)
-    # Make a prediction using the trained model
-   # prediction = model.predict(input_scaled)
-    # Inverse the transformation of the target variable
-   # prediction = yScaler.inverse_transform(prediction)
-   # return prediction
+# Create a dataframe from the input features
+# input_df = pd.DataFrame([features], columns=["symboling", "Fuel type", "aspiration", "doornumber", "Category", "Drive wheels", "enginelocation", "enginetype"])
+# One-hot encode the categorical columns
+#input_df = pd.get_dummies(input_df, columns=["symboling", "Fuel type", "aspiration", "doornumber", "Category", "Drive wheels", "enginelocation", "enginetype"])
+# Fill in the missing values for the numerical columns
+#input_df = input_df.fillna(input_df.mean())
+# Scale the input features
+#input_scaled = xScaler.transform(input_df)
+# Make a prediction using the trained model
+# prediction = model.predict(input_scaled)
+# Inverse the transformation of the target variable
+# prediction = yScaler.inverse_transform(prediction)
+# return prediction
 
-    # VERSION 1
-
-
-def predict_price(features):
-    # Create a dataframe from the input features
-    input_df = pd.DataFrame([features], columns=[ "Fuel type", "aspiration", "doornumber", "Category", "Cylinders", "Drive wheels", "enginetype"])
-    # One-hot encode the categorical columns
-    input_df = pd.get_dummies(input_df, columns=[ "Fuel type", "aspiration", "doornumber", "Category", "Cylinders", "Drive wheels", "enginetype"])
-    # Fill in the missing values for the numerical columns
-    input_df = input_df.fillna(0)
-    # Add remaining missing columns
-    for col in main_array.columns:
-        if col not in input_df.columns:
-            input_df[col] = 0
-    # Scale the input features
-    input_scaled = xScaler.transform(input_df)
-    # Make a prediction using the trained model
-    prediction = model.predict(input_scaled)
-    # Inverse the transformation of the target variable
-    prediction = yScaler.inverse_transform(prediction)
-    return prediction
+# VERSION 1
 
 # VERSION 2
+def preprocessing_data(df):
+
+    data2 = pd.read_csv(
+        "C:\\Users\\Mohammad\\Desktop\\Uni\\Uni work\\Year 4\\Term 7, fall 2022\\CMPE 460 Deep Learning\\project\\CMPE-460-Project\\dataset\\cars.csv",
+        encoding='iso-8859-1')
 
 
+    data2 = data2[data2['price'] >= 500]    
+
+    if 'Manufacturer' in data.columns:
+        data2 = data2.drop('Manufacturer', axis=1)
+    else:
+        print("Manufacturer column not found")
+
+    if 'Model ' in data.columns:
+        data2 = data2.drop('Model ', axis=1)
+    else:
+        print("Model  column not found")
 
 
+    data2 = data2[:-1]
+    data2 = data2.append(df)
 
+    categorical_cols = [
+        'symboling', 'Fuel type', 'aspiration', 'doornumber', 'Category',
+        'Drive wheels', 'enginelocation', 'enginetype', 'Cylinders',
+        'fuelsystem'
+    ]
+    main_array = np.array(data2['car_ID']).reshape(-1, 1)
+
+    # Iterate through columns in the dataframe
+    for col in data2.columns:
+        # Check if the column is in the list of categorical columns
+        if col in categorical_cols:
+            # Perform one-hot encoding on the column
+            temp = np.array(pd.get_dummies(data2[col]))
+        else:
+            # Otherwise, reshape the column
+            temp = np.array(data2[col]).reshape(-1, 1)
+        # Stack the column with the main array
+        main_array = np.hstack((main_array, temp))
+    print("preprocessign check")
+
+    main_array = main_array[:, 2:]
+    X_main = main_array[:, :-1]
+    X_main = X_main[:, :-1]
+
+    
+
+    predict_price(X_main)
+
+
+# predict_data = main_array[PREDICT_ROW, :].reshape(1, -1)
+# print(main_array)
+# X_predict = predict_data[:, :-1]
+# y_true = predict_data[:, -1]
+
+
+# preprocess the gui data to run it through the model
+def predict_price(df):
+
+    predict_data = df[-1, :].reshape(1, -1)
+
+    # last_row = df.iloc[-1]
+    # print(last_row)
+
+    print(df)
+    X_predict = predict_data[:, :-1]
+    y_true = predict_data[:, -1]
+
+    # xScaler = StandardScaler()
+    # yScaler = StandardScaler()
+
+
+    predict_data_scaled = xScaler.transform(X_predict)
+    y_pred_scaled = model.predict(predict_data_scaled)
+    y_pred = yScaler.inverse_transform(y_pred_scaled)
+    print("Prediction price result: {}".format(float(y_pred)))
+    print("True price: {}".format(float(y_true)))
+    print("Percentage error: {}".format(
+        str(float(abs(y_true - y_pred) * 100 / y_true))))
+
+    # return y_pred
--- code/gui2-0.py
+++ code/gui2-0.py
@@ -1,7 +1,8 @@
 from tkinter import *
 from tkinter import ttk
 import pandas as pd
-from NeuralNetwork1 import predict_price
+import numpy as np
+from NeuralNetwork1 import preprocessing_data
 # window attributes
 window = Tk()
 window.title("PriceMatch")
@@ -10,10 +11,12 @@ window.geometry('320x360')
 window.configure(background="#5A5A5A")
 
 data1 = pd.read_csv(
-    "C:\\Users\\Mohammad\\Desktop\\Uni\\Uni work\\Year 4\\Term 7, fall 2022\\CMPE 460 Deep Learning\\project\\CMPE-460-Project\\dataset\\cars.csv", encoding='latin1')
+    "C:\\Users\\Mohammad\\Desktop\\Uni\\Uni work\\Year 4\\Term 7, fall 2022\\CMPE 460 Deep Learning\\project\\CMPE-460-Project\\dataset\\cars.csv",
+    encoding='latin1')
 
 data = pd.read_csv(
-    "C:\\Users\\Mohammad\\Desktop\\Uni\\Uni work\\Year 4\\Term 7, fall 2022\\CMPE 460 Deep Learning\\project\\CMPE-460-Project\\dataset\\cars.csv", encoding='latin1')
+    "C:\\Users\\Mohammad\\Desktop\\Uni\\Uni work\\Year 4\\Term 7, fall 2022\\CMPE 460 Deep Learning\\project\\CMPE-460-Project\\dataset\\cars.csv",
+    encoding='latin1')
 
 data = data.groupby('Manufacturer')['Model '].apply(
     lambda x: x.unique().tolist()).reset_index()
@@ -22,7 +25,6 @@ data = data.groupby('Manufacturer')['Model '].apply(
 manufacturer_options = data['Manufacturer']
 # display the first item in the list in the drop-down menu
 manufactuer_var = manufacturer_options[0]
-
 
 # clean the data to store it into the different drop down menus
 
@@ -45,28 +47,23 @@ catagory_var = catagory_options[0]
 # list for different drive wheels options Drive wheels
 driveWheels_options = data1DriveWheels['Drive wheels']
 
-
 # list for different fuel types
 fuel_type_options = data1FuelType['Fuel type']
 fuel_type_var = fuel_type_options[0]
-
 
 # list for possible door numbers
 
 doorsNumber_options = data1DoorsNumber['doornumber']
 
-
 # list of possible number of cylinders
 
 cylinders_options = data1NumberOfCylinders['Cylinders']
-
 
 # list of possible engine aspirations
 engine_aspiration_options = data1Aspiration['aspiration']
 
 # list of possible engine types
 engine_type_options = data1EngineType['enginetype']
-
 
 # the string vars that are used to save data from the form
 aspiration_var = StringVar()
@@ -89,8 +86,8 @@ fuel_type = Label(window, text="fuel type").grid(row=5, column=0)
 cylinders = Label(window, text="number of cylinders").grid(row=7, column=0)
 driveWheels = Label(window, text="Drive wheels").grid(row=9, column=0)
 doorsNumber = Label(window, text="number of doors").grid(row=10, column=0)
-engineAspiration = Label(
-    window, text="enigne aspiration").grid(row=11, column=0)
+engineAspiration = Label(window, text="enigne aspiration").grid(row=11,
+                                                                column=0)
 engineType = Label(window, text="engine type").grid(row=12, column=0)
 # setting the display options
 manufactuer_var.set(manufacturer_options[0])
@@ -110,41 +107,42 @@ def on_manufacturer_select(event):
     models = data[data['Manufacturer'] ==
                   selected_manufacturer]['Model '].values[0]
     model_var.set(models[0])
-    model_entry = OptionMenu(window, model_var,  *models).grid(row=1, column=1)
+    model_entry = OptionMenu(window, model_var, *models).grid(row=1, column=1)
     model = Label(window, text="Model ").grid(row=1, column=0)
 
 
 # data entry forms
-manufactuer_dropDown = OptionMenu(
-    window, manufactuer_var, *manufacturer_options, command=on_manufacturer_select).grid(row=0, column=1)
+manufactuer_dropDown = OptionMenu(window,
+                                  manufactuer_var,
+                                  *manufacturer_options,
+                                  command=on_manufacturer_select).grid(
+                                      row=0, column=1)
 
-catagory_dropDown = OptionMenu(
-    window, catagory_var, *catagory_options).grid(row=3, column=1)
+catagory_dropDown = OptionMenu(window, catagory_var,
+                               *catagory_options).grid(row=3, column=1)
 
-fuel_type_entry = OptionMenu(
-    window, fuel_type_var, *fuel_type_options).grid(row=5, column=1)
+fuel_type_entry = OptionMenu(window, fuel_type_var,
+                             *fuel_type_options).grid(row=5, column=1)
 
+cylinders_entry = OptionMenu(window, cylinders_var,
+                             *cylinders_options).grid(row=7, column=1)
 
-cylinders_entry = OptionMenu(
-    window, cylinders_var, *cylinders_options).grid(row=7, column=1)
+driveWheels_entry = OptionMenu(window, driveWheels_var,
+                               *driveWheels_options).grid(row=9, column=1)
 
-driveWheels_entry = OptionMenu(
-    window, driveWheels_var, *driveWheels_options).grid(row=9, column=1)
+doorsNumber = OptionMenu(window, doorsNumber_var,
+                         *doorsNumber_options).grid(row=10, column=1)
 
-doorsNumber = OptionMenu(window, doorsNumber_var, *doorsNumber_options).grid(
-    row=10, column=1)
+doorsNumber = OptionMenu(window, aspiration_var,
+                         *engine_aspiration_options).grid(row=11, column=1)
 
-doorsNumber = OptionMenu(window, aspiration_var, *engine_aspiration_options).grid(
-    row=11, column=1)
-
-doorsNumber = OptionMenu(window, engineType_var, *engine_type_options).grid(
-    row=12, column=1)
-
+doorsNumber = OptionMenu(window, engineType_var,
+                         *engine_type_options).grid(row=12, column=1)
 
 # function to define the contentes of the model feild
 
-
 # submit function, takes data from the vars and saves them to variables
+
 
 def submit():
     manufacturer_data = manufactuer_var.get()
@@ -157,15 +155,44 @@ def submit():
     aspiration_data = aspiration_var.get()
     engine_type = engineType_var.get()
 
+    car_id = 206
+    symboling = 3
+    engine_location = "front"
+    wheel_base = 103.1
+    car_length = 192.7
+    car_width = 64.5
+    car_height = 48.8
+    car_weight = 2500
+    engine_size = 110
+    fuel_system = "mpfi"
+    bore_ratio = 2.68
+    stroke = 3.4
+    compression = 8.6
+    horesepower = 150
+    peakrpm = 5000
+    citympg = 24
+    highway_mpg = 28
+    price = 100
 
-# saving the data into a list from teh
-    list = [fuel_type_data, aspiration_data,doorsNumber_data, catagory_data,
-             cylinders_data, driveWheels_data, engine_type]
-    print(' '.join(map(str, list)))
+    list = [[car_id], [symboling], [fuel_type_data], [aspiration_data],
+            [doorsNumber_data], [catagory_data], [driveWheels_data],
+            [engine_location], [wheel_base], [car_length], [car_width],
+            [car_height], [car_weight], [engine_type], [cylinders_data],
+            [engine_size], [fuel_system], [bore_ratio], [stroke],
+            [compression], [horesepower], [peakrpm], [citympg], [highway_mpg],
+            [price]]
+    data = pd.DataFrame(list).transpose()
+    data.columns = [
+        'car_ID', 'symboling', 'Fuel type', 'aspiration', 'doornumber',
+        'Category', 'Drive wheels', 'enginelocation', 'wheelbase', 'carlength',
+        'carwidth', 'carheight', 'carweight', 'enginetype', 'Cylinders',
+        'enginesize', 'fuelsystem', 'boreratio', 'stroke', 'compressionratio',
+        'horsepower', 'peakrpm', 'citympg', 'highwaympg', 'price'
+    ]
 
-    prediction = predict_price(list)
-    print(prediction)
+    print(data)
 
+    preprocessing_data(data)
 
 
 def reset():
@@ -178,9 +205,8 @@ def reset():
     doorsNumber_var.set("")
 
 
-
-sub_btn = Button(window, text='submit featuers',
-                 command=submit).grid(row=14, column=1)
+sub_btn = Button(window, text='submit featuers', command=submit).grid(row=14,
+                                                                      column=1)
 sub_btn = Button(window, text='Reset', command=reset).grid(row=14, column=0)
 
 window.mainloop()
